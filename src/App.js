bbb9bd043b063226c3276694da83fd7f6e84b61b
Unknown <barchinsky@gmail.com>
2018-05-22 00:00:36 +0200

    Added proptypes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Theme, { createStyle } from 'react-native-theming';
+import { PropTypes } from 'prop-types';
 import themes from '@utils/themes';
 
-export default class App extends Component {
+import { logIn } from '@redux/actions/account';
+
+export class App extends Component {
 	componentDidMount() {
 		themes[0].apply();
+		//this.props.dispatch(logIn());
 	}
 
 	render() {
 		return (
 			<Theme.View style={styles.container}>
 				<Theme.Text style={styles.welcome}>Welcome to React Native!</Theme.Text>
-				<Theme.Text style={styles.instructions}>To get started, edit App.js</Theme.Text>
+				<Theme.Text style={styles.instructions}>
+					To get started, edit App.js
+				</Theme.Text>
 			</Theme.View>
 		);
 	}
@@ -19,8 +26,14 @@ export default class App extends Component {
 
 const styles = createStyle({
 	container: {
-		flex:1,
+		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center'
 	}
 });
+
+App.propTypes = {
+	dispatch: PropTypes.func
+};
+
+export default connect()(App);
