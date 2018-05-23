718e66b7c45f9939b3db7b82bbcc27f21d0a719f
Unknown <barchinsky@gmail.com>
2018-05-23 23:01:54 +0200

    Added styles

--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,8 +1,15 @@
 import { createTheme } from 'react-native-theming';
 
-const themes = [createTheme({
-	backgroundColor: '#ffffff',
-	textColor: '#000'
-}, 'Light')];
+const themes = [
+	createTheme(
+		{
+			backgroundColor: '#ffffff',
+			textColor: '#000',
+			placeholderTextColor: '#9e9e9e',
+			inputFieldBgColor: '#b1b1b1'
+		},
+		'Light'
+	)
+];
 
 export default themes;
