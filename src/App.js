da8086310d7038c8d6e4b4850df6c96bd9a58576
Unknown <barchinsky@gmail.com>
2018-05-27 14:07:23 +0200

    Integrated splash and Login screen into app

--- src/App.js
+++ src/App.js
@@ -5,19 +5,57 @@ import { PropTypes } from 'prop-types';
 import themes from '@utils/themes';
 
 import LoginScreen from '@screens/Login/Login';
+import SplashScreen from '@screens/Splash/Splash';
+import HomeScreen from '@screens/Home/Home';
+
+import { getUserData } from '@api/local-storage';
 
 import { logIn } from '@redux/actions/account';
 
 export class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			isLoaded: false,
+			isLoggedIn: false,
+		};
+	}
+
 	componentDidMount() {
 		themes[0].apply();
 		//this.props.dispatch(logIn());
+		// fetch locally saved user data
+		getUserData()
+			.then(userData => {
+				this.setState({ isLoaded: true, isLoggedIn: !!userData.userId });
+			});
 	}
+
+	static getDerivedStateFromProps(props, state) {
+		if (props.userData.userId && state.isLoggedIn === false) {
+			return { isLoggedIn: true };
+		}
+
+		return null;
+	}
+
+	renderStartScreen = () => {
+		const { isLoaded, isLoggedIn } = this.state;
+
+		if (!isLoaded) {
+			return <SplashScreen />;
+		} else if (isLoggedIn) {
+			return <HomeScreen />;
+		} else {
+			return <LoginScreen />;
+		}
+	};
 
 	render() {
 		return (
 			<Theme.View style={styles.container}>
-				<LoginScreen />
+				{this.renderStartScreen()}
 			</Theme.View>
 		);
 	}
@@ -25,12 +63,19 @@ export class App extends Component {
 
 const styles = createStyle({
 	container: {
-		flex: 1
-	}
+		flex: 1,
+	},
 });
 
 App.propTypes = {
-	dispatch: PropTypes.func
+	dispatch: PropTypes.func,
+	userData: PropTypes.object,
 };
 
-export default connect()(App);
+function mapStateToProps(state) {
+	return {
+		userData: state.userData,
+	};
+}
+
+export default connect(mapStateToProps)(App);
