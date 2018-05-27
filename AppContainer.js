823b1954481fef736b9a033bdc5658d53537c257
Unknown <barchinsky@gmail.com>
2018-05-27 14:12:23 +0200

    Added debug stuff

--- AppContainer.js
+++ AppContainer.js
@@ -4,11 +4,44 @@ import App from './src/App';
 
 import store from '@redux/store';
 
+import { login } from '@api/authorization';
+import {
+	getUserData,
+	saveUserLoginAndToken,
+	clearLocalCreds,
+} from '@api/local-storage';
+import { logIn } from '@redux/actions/account';
+
+// saveUserLoginAndToken('1', '2')
+// 	.then(err => {
+// 		console.log('------------->', err);
+// 	});
+
+// getUserData()
+// 	.then(data => {
+// 		console.log('Local user data:', data);
+// 	});
+
+// login('test', '12345')
+// 	.then(res => {
+// 		console.log(res);
+// 	})
+// 	.catch(err => {
+// 		console.log(err);
+// 	});
+//clearLocalCreds();
+
+//store.dispatch(logIn('test', '12345', true));
+
+store.subscribe(() => {
+	console.log(store.getState());
+});
+
 export default class AppContainer extends React.Component {
 	render() {
 		return (
 			<Provider store={store}>
-				<App/>
+				<App />
 			</Provider>
 		);
 	}
