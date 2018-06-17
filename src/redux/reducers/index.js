1cd27e45b8930ec66418c5c88559cff550775ca1
Unknown <barchinsky@gmail.com>
2018-06-17 16:30:35 +0200

    Added fetchCategories redux actions and reducers

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -2,8 +2,10 @@ import { combineReducers } from 'redux';
 
 import accountReducer from '@redux/reducers/account';
 import budgetsReducer from '@redux/reducers/budgets';
+import categoriesReducer from '@redux/reducers/category';
 
 export default combineReducers({
 	userData: accountReducer,
 	budgets: budgetsReducer,
+	categories: categoriesReducer,
 });
