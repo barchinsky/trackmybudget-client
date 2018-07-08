aa05d549f6ce02d2a06da7b012016efd7324e84f
Unknown <barchinsky@gmail.com>
2018-07-08 21:54:34 +0200

    Disabled load user data and added debug

--- src/screens/Home/Home.js
+++ src/screens/Home/Home.js
@@ -35,21 +35,21 @@ export class App extends Component {
 	}
 
 	loadUserData = async () => {
-		const userData = await getUserData();
-		if (!userData.userId) {
-			console.log('userData.userId:', userData.userId);
-			this.props.navigation.navigate('LoginScreen');
-		} else {
-			this.props.dispatch(loginSuccess(userData));
+		// const userData = await getUserData();
+		// if (!userData.userId) {
+		// 	console.log('userData.userId:', userData.userId);
+		// 	this.props.navigation.navigate('LoginScreen');
+		// } else {
+		// this.props.dispatch(loginSuccess(userData));
 
-			const initStack = [
-				this.props.dispatch(fetchBudgets()),
-				this.props.dispatch(fetchCategories()),
-			];
-			await Promise.all(initStack);
+		// const initStack = [
+		// 	this.props.dispatch(fetchBudgets()),
+		// 	this.props.dispatch(fetchCategories()),
+		// ];
+		// await Promise.all(initStack);
 
-			this.setState({ isLoaded: true, isLoggedIn: true });
-		}
+		this.setState({ isLoaded: true, isLoggedIn: true });
+		// }
 	};
 
 	static getDerivedStateFromProps(props, state) {
