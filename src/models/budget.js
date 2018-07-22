241411aee080abfee89a08851b997cff362a0a62
barchinsky <barchinsky@gmail.com>
2018-07-22 22:31:21 +0200

    Implemented serialization/deserialization methods

--- src/models/budget.js
+++ src/models/budget.js
@@ -2,14 +2,23 @@ import moment from 'moment';
 import { datetime } from '@utils/dateFormats';
 
 export default class Budget {
-	constructor(raw_data) {
-		this.id = raw_data._id;
+	constructor(raw_data = {}) {
+		this.id = raw_data.id;
 		this.name = raw_data.name;
 		this.startDate = moment(raw_data.startDate)
 			.format(datetime);
 		this.endDate = moment(raw_data.endDate)
 			.format(datetime);
-		this.spentAmount = raw_data.spentAmount;
-		this.estimate = raw_data.estimate;
+		this.spentAmount = raw_data.spentAmount || 0;
+		this.estimate = raw_data.estimate || 0;
 	}
+
+	serialize = () => {
+		return JSON.stringify(this);
+	};
+
+	static deserialize = str => {
+		const deserialized = JSON.parse(str);
+		return new Budget(deserialized);
+	};
 }
