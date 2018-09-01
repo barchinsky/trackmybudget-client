d6854dfed0bc5a89cd89cfb4044bc44d49a5d104
Unknown <barchinsky@gmail.com>
2018-09-01 18:54:54 +0200

    Removed moment formatting

--- src/models/budget.js
+++ src/models/budget.js
@@ -1,14 +1,9 @@
-import moment from 'moment';
-import { datetime } from '@utils/dateFormats';
-
 export default class Budget {
 	constructor(raw_data = {}) {
 		this.id = raw_data.id;
 		this.name = raw_data.name;
-		this.startDate = moment(raw_data.startDate)
-			.format(datetime);
-		this.endDate = moment(raw_data.endDate)
-			.format(datetime);
+		this.startDate = raw_data.startDate;
+		this.endDate = raw_data.endDate;
 		this.spentAmount = raw_data.spentAmount || 0;
 		this.estimate = raw_data.estimate || 0;
 		this.categoryIdToEstimateMap = raw_data.categoryIdToEstimateMap || {};
