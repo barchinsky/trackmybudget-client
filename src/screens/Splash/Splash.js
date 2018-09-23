aef0b76777ffdf22f750ad268354faaf10cc79fa
Unknown <barchinsky@gmail.com>
2018-09-23 18:17:38 +0200

    Added version to splash screen

--- src/screens/Splash/Splash.js
+++ src/screens/Splash/Splash.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import Theme from 'react-native-theming';
+import * as pack from 'trackmybudget/package.json';
 
 import styles from './_styles.js';
 
 export default class SplashScreen extends Component {
+	renderVersion = () => {
+		return <Theme.Text style={styles.versionText}>{pack.version}</Theme.Text>;
+	};
 	render() {
 		return (
 			<Theme.View style={styles.container}>
 				<Theme.Text style={styles.splashLogoText}>Track My Budget</Theme.Text>
+				{this.renderVersion()}
 			</Theme.View>
 		);
 	}
